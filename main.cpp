040a0988181a375407fb73bfc179d0fac298e648
JiangZiyang0120 <yangzejia0120@qq.com>
2021-11-19 08:02:36 -0800

    change some function

--- main.cpp
+++ main.cpp
@@ -9,11 +9,7 @@
 #include "util_algorithm.h"
 #include "linear_model.h"
 
-using namespace std;
 
-int main(){
-    Matrix a = {{1.2, 3.4, 5.6},
-                {2.1,2.3,3.3},
-                {1.2,2.0,2.7}};
-    cout<<inv(a*~a)<<endl;
+int main() {
+
 }
--- test/test.cpp
+++
@@ -1,6 +0,0 @@
-//
-// Created by jiang-ziyang on 2021/10/25.
-//
-
-#include <stdio.h>
-#include <math.h>
